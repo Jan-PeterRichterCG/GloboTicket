38ba1c683181b522d9e226e0a94486d8957d11c2
Jan-PeterRichterCG <jan-peter.richter@capgemini.com>
2021-08-11 15:53:47 +0200

    restored authorization (is active in CategoryController only anyway) and added more logging to CategoryController

--- GloboTicket.TicketManagement.Api/Controllers/CategoryController.cs
+++ GloboTicket.TicketManagement.Api/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -15,18 +16,25 @@ namespace GloboTicket.TicketManagement.Api.Controllers
     public class CategoryController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly ILogger _logger;
 
-        public CategoryController(IMediator mediator)
+        public CategoryController(IMediator mediator, ILogger<CategoryController> logger)
         {
             _mediator = mediator;
+
+            _logger = logger;
         }
 
-        //[Authorize]
+        [Authorize]
         [HttpGet("all", Name = "GetAllCategories")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<List<CategoryListVm>>> GetAllCategories()
         {
+            _logger.LogDebug("GetAllCategories...");
+
             var dtos = await _mediator.Send(new GetCategoriesListQuery());
+
+            _logger.LogDebug($"Found Categories: {@dtos.Count}");
             return Ok(dtos);
         }
 
@@ -36,16 +44,26 @@ namespace GloboTicket.TicketManagement.Api.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<List<CategoryEventListVm>>> GetCategoriesWithEvents(bool includeHistory)
         {
+            _logger.LogDebug($"GetAllCategories with events (includeHistory = {@includeHistory})...");
+
             GetCategoriesListWithEventsQuery getCategoriesListWithEventsQuery = new GetCategoriesListWithEventsQuery() { IncludeHistory = includeHistory };
 
             var dtos = await _mediator.Send(getCategoriesListWithEventsQuery);
+
+            _logger.LogDebug($"Found Categories (with events): {@dtos.Count}");
+
             return Ok(dtos);
         }
 
         [HttpPost(Name = "AddCategory")]
         public async Task<ActionResult<CreateCategoryCommandResponse>> Create([FromBody] CreateCategoryCommand createCategoryCommand)
         {
+            _logger.LogDebug($"AddCategory {@createCategoryCommand.Name}...");
+
             var response = await _mediator.Send(createCategoryCommand);
+
+            _logger.LogDebug($"Category {@response.Category.Name} created...");
+
             return Ok(response);
         }
     }
