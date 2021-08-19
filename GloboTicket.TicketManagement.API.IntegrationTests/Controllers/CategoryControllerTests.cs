bf4a67146c08b09d22cbdbe3a30bc76190b7b260
Jan-PeterRichterCG <jan-peter.richter@capgemini.com>
2021-08-19 16:17:50 +0200

    A test case for Add Category. Unfortunately, the tests are actually running against the SQL Server DB, not the InMemory DB, so the newly created Categories clutter the DB :-(

--- GloboTicket.TicketManagement.API.IntegrationTests/Controllers/CategoryControllerTests.cs
+++ GloboTicket.TicketManagement.API.IntegrationTests/Controllers/CategoryControllerTests.cs
@@ -2,9 +2,11 @@ using GloboTicket.TicketManagement.Api;
 using GloboTicket.TicketManagement.API.IntegrationTests.Base;
 using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
 using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents;
+using GloboTicket.TicketManagement.Application.Features.Categories.Commands.CreateCateogry;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Net.Http;
 using Xunit;
 
 namespace GloboTicket.TicketManagement.API.IntegrationTests.Controllers
@@ -88,5 +90,40 @@ namespace GloboTicket.TicketManagement.API.IntegrationTests.Controllers
             }
         }
 
+        [Fact]
+        public async Task SuccessfulAddCategory()
+        {
+            var client = await _factory.GetAuthenticatedClientAsync();
+
+            // phase I: create the category
+            var createRequest = new CreateCategoryCommand();
+            createRequest.Name = "TestCategory"+System.DateTime.Now.Ticks.ToString();
+
+            var request = JsonConvert.SerializeObject(createRequest);
+
+            var response = await client.PostAsync("/api/category", new StringContent(request, System.Text.Encoding.UTF8, "application/json"));
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var createResponse = JsonConvert.DeserializeObject<CreateCategoryCommandResponse>(responseString);
+
+            Assert.Equal(createRequest.Name, createResponse.Category.Name);
+
+            // phase II: check whether the category is now part of the list of categories
+
+            var checkResponse = await client.GetAsync("/api/category/all");
+
+            checkResponse.EnsureSuccessStatusCode();
+
+            var checkResponseString = await checkResponse.Content.ReadAsStringAsync();
+
+            var result = JsonConvert.DeserializeObject<List<CategoryListVm>>(checkResponseString);
+            
+            Assert.IsType<List<CategoryListVm>>(result);
+            Assert.NotEmpty(result);
+            var newCategory = result.Find(c => c.Name.Equals(createRequest.Name));
+            Assert.NotNull(newCategory);
+        }
     }
 }
